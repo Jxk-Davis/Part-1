40999a92c75e8ae91cde976fb9431bc793414307
Jxk-Davis <lovingpotion@gmail.com>
2024-01-17 11:01:06 -0500

    Task 2 Complete: trigger & colour changes

---
+++ Assets/Week 2/Scripts/Switch.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    SpriteRenderer sprRend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprRend = GetComponent<SpriteRenderer>();
+        sprRend.color = Color.yellow;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("what's happening, " + collision.gameObject + " eh?");
+        sprRend.color = Color.red;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("later, " + collision.gameObject);
+        sprRend.color = Color.yellow;
+    }
+}
