6ce26105d1046cd300bbc1ff657bbde4c5332ef2
Jxk-Davis <juliannaxkd@gmail.com>
2024-02-06 21:49:04 -0500

    button finished

--- Assets/Assignment/Scripts/Button.cs
+++ Assets/Assignment/Scripts/Button.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
+    public GameObject gem;
+    public GameObject gemSpawn;
+    SpriteRenderer sprRend;
+    public bool pressed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sprRend = GetComponent<SpriteRenderer>();
+        sprRend.color = Color.green;
+        pressed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(pressed == false)
+        {
+            Instantiate(gem, gemSpawn.transform.position, Quaternion.identity);
+            sprRend.color = Color.red;
+            pressed = true;
+        }
     }
 }
