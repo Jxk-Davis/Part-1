bf202c3cb3ed20e14bd7869bc07644b4c7d714e1
Jxk-Davis <juliannaxkd@gmail.com>
2024-02-06 19:26:33 -0500

    player movement done

--- Assets/Assignment/Scripts/MovingPlatform.cs
+++ Assets/Assignment/Scripts/MovingPlatform.cs
@@ -6,16 +6,18 @@ public class MovingPlatform : MonoBehaviour
 {
     public GameObject pointA;
     public GameObject pointB;
+    public float spe;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //get transform components of point A and point B
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //move up/down by spe
+        //check each frame if y transform equals or exceeds that of Point A or Point B, if so multiply spe by -1
     }
 }
--- Assets/Assignment/Scripts/Platformer.cs
+++ Assets/Assignment/Scripts/Platformer.cs
@@ -6,10 +6,11 @@ public class Platformer : MonoBehaviour
 {
     public float maxSpe;
     public float jumpForce;
-    public float walkForce;
+    private Vector3 jumpTeleport;
+    public float accel;
     Vector2 direction;
-    private bool grounded;
-    private bool jumping;
+    public bool grounded;
+    public bool jumping;
     private Rigidbody2D rb2d;
 
     // Start is called before the first frame update
@@ -20,12 +21,45 @@ public class Platformer : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (rb2d.velocity.magnitude < maxSpe)
-        { rb2d.AddForce(direction * walkForce * Time.deltaTime); }
+        Move();
+        Jump();
     }
     // Update is called once per frame
     void Update()
     {
+        Jump();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumping = true;
+        }
         direction.x = Input.GetAxis("Horizontal");
+    }
+
+    private void Jump()
+    { 
+        if(grounded == true)// check if we are grounded. if so...
+        {
+            if (jumping == true) //check if space is pressed. if so...
+            {
+                rb2d.AddForce(Vector2.up * jumpForce); // get pushed up by jumpForce
+                grounded = false;// set grounded to false
+                jumping = false; //set jumping to false
+            }
+        }
+    }
+
+    private void Move()
+    {
+        if(grounded == true) //check if we are on the ground. if so...
+        {
+            if (rb2d.velocity.magnitude < maxSpe) // check that we are under the speed limit. if so...
+            { rb2d.AddForce(direction * accel * Time.deltaTime); } // accelerate in the direction of Horizontal axis
+        } else //otherwise...
+        { rb2d.AddForce(direction * (accel/2) * Time.deltaTime); }// accelerate in the direction of Horizontal axis
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        grounded = true;
     }
 }
